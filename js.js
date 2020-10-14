869d79df87ed03d6157a1e31c726625b99263c91
Cadertea <cadertea@ncsu.edu>
2020-10-14 19:44:27 -0400

    worked on getting a informative country api to work need to work on some esthetic bug

--- js.js
+++ js.js
@@ -14,6 +14,32 @@ $("#search").on('click', function(){
     populationEl.style.display = 'block';
     attractionEl.style.display = 'block';
     flightsEl.style.display = 'block';
-
+    var country = $("#city-input").val()
+    // console.log(cityInput)
+    query3(country);
 
 })
+
+function query3(country) {
+
+
+    var queryURL3 = "https://restcountries.eu/rest/v2/name/"+ country + "?fullText=true"
+    // console.log(queryURL3);
+    $.ajax({
+      url: queryURL3,
+      method: "GET"
+    })
+  
+      .then(function (response3) {
+        console.log(response3);
+     
+        var countrypop = response3[0].population
+        $('#value-1').html('<h3>' +'Population: ' + countrypop)
+      
+        var src = response3[0].flag;
+        var img = $('<img>')
+       var total =  img.attr('src' , src)
+        $('#countryflag').append(total)
+      });
+  
+  }
