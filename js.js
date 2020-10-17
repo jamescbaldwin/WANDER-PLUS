b1de35b080f26ca82b7c19d1fd9ebc6c91b3adf9
Cadertea <cadertea@ncsu.edu>
2020-10-17 10:12:28 -0400

    added code from everyone and placed comments so we know who wrote each line of code

--- js.js
+++ js.js
@@ -45,3 +45,19 @@ function query3(country) {
       });
 
   }
+
+// james code for the slidebar
+  var mini = true;
+function toggleSidebar() {
+  if (mini) {
+    console.log("opening sidebar");
+    document.getElementById("mySidebar").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+    this.mini = false;
+  } else {
+    console.log("closing sidebar");
+    document.getElementById("mySidebar").style.width = "40px";
+    document.getElementById("main").style.marginLeft = "40px";
+    this.mini = true;
+  }
+}
